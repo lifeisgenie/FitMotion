dfc93897ccb843192012e5a6f1f195b02848d6f4
서강 <gangs@seogang-ui-MacBookPro.local>
2024-06-10 17:44:01 +0900

    [FE] 텍스트수정

--- Frontend/lib/pages/setting.dart
+++ Frontend/lib/pages/setting.dart
@@ -86,7 +86,7 @@ class _SettingsPageState extends State<SettingsPage> {
           ListTile(
             leading: Icon(Icons.person, color: Colors.white),
             title: Text(
-              '프로필 수정',
+              '비밀번호 변경',
               style: TextStyle(fontSize: 21, color: Colors.white),
             ),
             trailing: Icon(Icons.arrow_forward_ios, color: Colors.white),
